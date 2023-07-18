f65e51745fa45431d57fbe19841b893a81c41d84
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-07-18 17:36:43 +0300

    change slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -3,23 +3,35 @@ import s from "./Slider.module.scss";
 
 const Slider = () => {
   return (
-    <Carousel>
-      <Carousel.Item interval={2000}>
-        <div className={s.cart}>
-          <h4 className={s.name}>Юрій Полупан</h4>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <div className={s.cart}>
-          <h4 className={s.name}>Олександр Білик</h4>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className={s.cart}>
-          <h4 className={s.name}>Олександр Новіков</h4>
-        </div>
-      </Carousel.Item>
-    </Carousel>
+    <div className={s.slider}>
+      <Carousel>
+        <Carousel.Item interval={2000}>
+          <div className={s.cart}>
+            <h4 className={s.name}>Юрій Полупан</h4>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className={s.cart}>
+            <h4 className={s.name}>Олександр Білик</h4>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className={s.cart}>
+            <h4 className={s.name}>Олександр Новіков</h4>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className={s.cart}>
+            <h4 className={s.name}>Юрій Гарячев</h4>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className={s.cart}>
+            <h4 className={s.name}>Михаїл Фесенко</h4>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 };
 

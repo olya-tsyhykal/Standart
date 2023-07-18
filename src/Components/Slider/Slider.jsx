ff3ef99f3b16be56e6988449ca9d602d2f510fa3
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-07-18 17:11:20 +0300

    change interval

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,21 +1,20 @@
 import Carousel from "react-bootstrap/Carousel";
-import slide from "../../Shared/Images/mag.jpg";
 import s from "./Slider.module.scss";
 
 const Slider = () => {
   return (
     <Carousel>
-      <Carousel.Item interval={1500}>
+      <Carousel.Item interval={2000}>
         <div className={s.cart}>
           <h4 className={s.name}>Юрій Полупан</h4>
         </div>
       </Carousel.Item>
-      <Carousel.Item interval={1500}>
+      <Carousel.Item interval={1000}>
         <div className={s.cart}>
           <h4 className={s.name}>Олександр Білик</h4>
         </div>
       </Carousel.Item>
-      <Carousel.Item interval={1500}>
+      <Carousel.Item>
         <div className={s.cart}>
           <h4 className={s.name}>Олександр Новіков</h4>
         </div>

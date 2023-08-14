2ee21514ffd20406770780cb70193cbb2ebc7e62
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-08-14 17:02:25 +0300

    change login form and add validation

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import TextField from "../TextField/TextField";
-import { fields } from "../TextField/fields";
+
 import s from "./LoginForm.module.scss";
 
 const LoginForm = ({ onSubmit }) => {
@@ -41,27 +40,44 @@ const LoginForm = ({ onSubmit }) => {
   return (
     <form onSubmit={hendleSubmit} className={s.loginForm}>
       <div className={s.form}>
-      
-        <TextField
-          value={email}
-          onChange={hendleInputChange}
-          {...fields.email}
-          className={s.formInput}
-        />
-        <div className={s.formText}>
-          введіть корректний emil. Наприклад - polupan@mail.com
+        <div className={s.formItem}>
+          <input
+            value={email}
+            onChange={hendleInputChange}
+            className={s.formInput}
+            type="email"
+            name="email"
+            id="email"
+            required
+            pattern="\S+@[a-z]+\.[a-z]+"
+            title="The email must contain only Latin lowercase letters, @ and  a dot without spacesю  For example - barbos@mail.com"
+          />
+          <label className={s.formLabel} htmlFor="email">
+            Email
+          </label>
+          <p className={s.formText}>
+            *введіть корректний emil. Наприклад - barbos@gmail.com
+          </p>
         </div>
-        
-        <TextField
-          value={password}
-          onChange={hendleInputChange}
-          {...fields.password}
-          className={s.formInput}
-        />
-        <div className={s.formText}>
-          cannot be less than 8 characters and must contain at least one number,
-          one lowercase, and one uppercase Latin letter. For example -
-          Butterfly01
+        <div className={s.formItem}>
+          <input
+            value={password}
+            onChange={hendleInputChange}
+            className={s.formInput}
+            type="password"
+            name="password"
+            id="password"
+            required
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="The password cannot be less than 8 characters and must contain at least one number, one lowercase, and one uppercase Latin letter. For example - Butterfly01"
+          />
+          <label className={s.formLabel} htmlFor="password">
+            Password
+          </label>
+          <p className={s.formText}>
+            *не може бути менше 8 символів, має містити <br />принаймні одне число,
+            одну малу і одну велику <br />латинську літеру. Наприклад - Butterfly01
+          </p>
         </div>
       </div>
       <button type="submit" className={s.button}>

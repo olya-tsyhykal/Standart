e7e540fb64d2b1d6b727d65853b070509ede32fc
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-08-28 12:11:19 +0300

    change style contacts map

--- src/Components/Contacts/Contacts.jsx
+++ src/Components/Contacts/Contacts.jsx
@@ -2,7 +2,7 @@ import s from "./Contacts.module.scss";
 
 const Contacts = () => {
   return (
-    <section id="contacts">
+    <section id="contacts" >
       <h2 className={s.title}>Контакти</h2>
       <div className={s.contacts}>
         <div className={s.contactsContainer}>
@@ -32,6 +32,7 @@ const Contacts = () => {
             <p className={s.dayOfWork}>Сб-Нд 09:00-14:00 </p>
           </div>
         </div>
+        <div className={s.map}>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2591.0231329040785!2d36.56021527562302!3d49.5029571551321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x412759c0cf3f4443%3A0x7e76a63fa0e55615!2z0LLRg9C7LiDQktC10YHQvdGP0L3QsCwg0JTQvtC90LXRhtGMLCDQpdCw0YDQutGW0LLRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgNjQyNTA!5e0!3m2!1suk!2sua!4v1689167168598!5m2!1suk!2sua"
           width="400"
@@ -41,6 +42,7 @@ const Contacts = () => {
           referrerPolicy="no-referrer-when-downgrade"
           className={s.googleMap}
         ></iframe>
+        </div>
       </div>
     </section>
   );

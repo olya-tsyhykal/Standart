3a4ac213f126699a334243a096f3c8cd934d6032
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-09-05 09:46:00 +0300

    change firebase project, firebase config and add cursor pointer to navigation menu

--- src/Shared/firebase/config.js
+++ src/Shared/firebase/config.js
@@ -1,17 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAonQTFuujzl6XrOoFAcr4ttQSBjiH_aRA",
-  authDomain: "debstat-d9c5c.firebaseapp.com",
-  projectId: "debstat-d9c5c",
-  storageBucket: "debstat-d9c5c.appspot.com",
-  messagingSenderId: "603277078796",
-  appId: "1:603277078796:web:4dccc3ee2981264693d929",
-  measurementId: "G-P31WHTNNG8",
+  // apiKey: "AIzaSyAonQTFuujzl6XrOoFAcr4ttQSBjiH_aRA",
+  // authDomain: "debstat-d9c5c.firebaseapp.com",
+  // projectId: "debstat-d9c5c",
+  // storageBucket: "debstat-d9c5c.appspot.com",
+  // messagingSenderId: "603277078796",
+  // appId: "1:603277078796:web:4dccc3ee2981264693d929",
+  // measurementId: "G-P31WHTNNG8",
+  apiKey: "AIzaSyCJLo1Rvph0AZ8cd3VpPkrXqaeKtqoKWvg",
+  authDomain: "standart-e8f58.firebaseapp.com",
+  projectId: "standart-e8f58",
+  storageBucket: "standart-e8f58.appspot.com",
+  messagingSenderId: "596218549966",
+  appId: "1:596218549966:web:1ee533ef75979dc635229f",
+  measurementId: "G-GFPGMXLP3Z"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+// export const db = getFirestore(app);
